348f99a4418191c24c32c5b6a468b6f6112847b2
Jimmy Callin <jimmy.callin@gmail.com>
2021-07-20 02:20:04 +0200

    Fix babel issues in building process

--- dist/index.js
+++ dist/index.js
@@ -5,36 +5,30 @@ var React__default = _interopDefault(React);
 var videojs = _interopDefault(require('video.js'));
 require('video.js/dist/video-js.css');
 
-var useVideoJS = function useVideoJS(videoJsOptions) {
-  var videoNode = React.useRef(null);
-
-  var _useState = React.useState(false),
-      ready = _useState[0],
-      setReady = _useState[1];
-
-  var changedKey = JSON.stringify(videoJsOptions);
-  var player = React.useRef(null);
-  React.useEffect(function () {
+const useVideoJS = videoJsOptions => {
+  const videoNode = React.useRef(null);
+  const [ready, setReady] = React.useState(false);
+  const changedKey = JSON.stringify(videoJsOptions);
+  const player = React.useRef(null);
+  React.useEffect(() => {
     player.current = videojs(videoNode.current, videoJsOptions);
-    player.current.ready(function () {
+    player.current.ready(() => {
       setReady(true);
     });
-    return function () {
+    return () => {
       player.current.dispose();
     };
   }, [changedKey]);
-  var Video = React.useCallback(function () {
-    return /*#__PURE__*/React__default.createElement("div", {
-      "data-vjs-player": true,
-      key: changedKey
-    }, /*#__PURE__*/React__default.createElement("video", {
-      ref: videoNode,
-      className: "video-js"
-    }));
-  }, [changedKey]);
+  const Video = React.useCallback(() => /*#__PURE__*/React__default.createElement("div", {
+    "data-vjs-player": true,
+    key: changedKey
+  }, /*#__PURE__*/React__default.createElement("video", {
+    ref: videoNode,
+    className: "video-js"
+  })), [changedKey]);
   return {
-    Video: Video,
-    ready: ready,
+    Video,
+    ready,
     player: player.current
   };
 };
--- dist/index.modern.js
+++ dist/index.modern.js
@@ -2,36 +2,30 @@ import React, { useRef, useState, useEffect, useCallback } from 'react';
 import videojs from 'video.js';
 import 'video.js/dist/video-js.css';
 
-var useVideoJS = function useVideoJS(videoJsOptions) {
-  var videoNode = useRef(null);
-
-  var _useState = useState(false),
-      ready = _useState[0],
-      setReady = _useState[1];
-
-  var changedKey = JSON.stringify(videoJsOptions);
-  var player = useRef(null);
-  useEffect(function () {
+const useVideoJS = videoJsOptions => {
+  const videoNode = useRef(null);
+  const [ready, setReady] = useState(false);
+  const changedKey = JSON.stringify(videoJsOptions);
+  const player = useRef(null);
+  useEffect(() => {
     player.current = videojs(videoNode.current, videoJsOptions);
-    player.current.ready(function () {
+    player.current.ready(() => {
       setReady(true);
     });
-    return function () {
+    return () => {
       player.current.dispose();
     };
   }, [changedKey]);
-  var Video = useCallback(function () {
-    return /*#__PURE__*/React.createElement("div", {
-      "data-vjs-player": true,
-      key: changedKey
-    }, /*#__PURE__*/React.createElement("video", {
-      ref: videoNode,
-      className: "video-js"
-    }));
-  }, [changedKey]);
+  const Video = useCallback(() => /*#__PURE__*/React.createElement("div", {
+    "data-vjs-player": true,
+    key: changedKey
+  }, /*#__PURE__*/React.createElement("video", {
+    ref: videoNode,
+    className: "video-js"
+  })), [changedKey]);
   return {
-    Video: Video,
-    ready: ready,
+    Video,
+    ready,
     player: player.current
   };
 };
