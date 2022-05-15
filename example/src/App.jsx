3145d3bdc2cfd41987ef01071be47ef713fee42b
Jimmy Callin <jimmy.callin@gmail.com>
2022-05-16 00:13:10 +0200

    strictmode working version

--- example/src/App.jsx
+++ example/src/App.jsx
@@ -1,33 +1,27 @@
 import React, { useState } from "react";
 import "./App.css";
 
-import { useVideoJS } from "react-hook-videojs";
+// import { useVideoJS } from "react-hook-videojs";
+import { useVideoJS } from "../../src/index.jsx";
 import "video.js/dist/video-js.css";
 
 const App = () => {
   const [source, setSource] = useState("//vjs.zencdn.net/v/oceans.mp4");
   const [controls, setControls] = useState(true);
   const [autoplay, setAutoplay] = useState(false);
+  const [isMount, setIsMount] = useState(true);
 
   const videoJsOptions = {
     sources: [{ src: source }],
     controls,
     autoplay,
   };
-  const className = "my-class";
+  const className = "my-class ";
   const { Video, ready, player } = useVideoJS(videoJsOptions, className);
   console.log({ Video, ready, player });
   return (
     <>
-      <Video>
-        <track
-          kind="captions"
-          src="//example.com/path/to/captions.vtt"
-          srcLang="en"
-          label="English"
-          default
-        />
-      </Video>
+      {isMount && <Video />}
       <div style={{ display: "flex", flexDirection: "column", margin: "20px" }}>
         <label>
           Video source
@@ -52,6 +46,14 @@ const App = () => {
             type="checkbox"
             checked={autoplay}
             onChange={(e) => setAutoplay(e.target.checked)}
+          />
+        </label>
+        <label>
+          Mounted
+          <input
+            type="checkbox"
+            checked={isMount}
+            onChange={() => setIsMount((m) => !m)}
           />
         </label>
       </div>
--- src/index.jsx
+++ src/index.jsx
@@ -1,36 +1,58 @@
+/* eslint-disable react/prop-types */
 import React, { useRef, useState, useEffect, useCallback } from "react";
 import videojs from "video.js";
-
+import "video.js/dist/video-js.css";
 export const useVideoJS = (videoJsOptions, classNames = "") => {
   const [ready, setReady] = useState(false);
   const changedKey = JSON.stringify(videoJsOptions);
   const videoNode = useRef(null);
   const player = useRef(null);
+  const containerRef = useRef(null);
+
   useEffect(() => {
-    player.current = videojs(videoNode.current, videoJsOptions);
-    player.current.ready(() => {
-      setReady(true);
-    });
-    return () => {
-      player.current.dispose();
+    () => {
+      if (player.current) {
+        player.current.dispose();
+        player.current = null;
+      }
     };
-  }, [changedKey]);
+  }, []);
 
   const Video = useCallback(
     ({ children, ...props }) => {
+      useEffect(() => {
+        if (videoNode.current && containerRef.current) {
+          if (!containerRef.current.contains(videoNode.current.parentNode)) {
+            containerRef.current.appendChild(videoNode.current.parentNode);
+          }
+
+          player.current = videojs(videoNode.current, videoJsOptions);
+          player.current.ready(() => {
+            setReady(true);
+          });
+        }
+        return () => {
+          if (player.current) {
+            setReady(false);
+            player.current.reset();
+          }
+        };
+      }, [changedKey]);
       return (
-        <div data-vjs-player key={changedKey}>
-          <video
-            ref={videoNode}
-            className={`video-js ${classNames}`}
-            {...props}
-          >
-            {children}
-          </video>
+        <div ref={containerRef}>
+          <div data-vjs-player>
+            <video
+              ref={videoNode}
+              className={`video-js ${classNames}`}
+              {...props}
+            >
+              {children}
+            </video>
+          </div>
         </div>
       );
     },
-    [changedKey]
+    [classNames, changedKey]
   );
   return { Video, ready, player: player.current };
 };
