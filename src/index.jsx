8b80161f6a1f93e245b9937517e003ff20cca905
Jimmy Callin <jimmy.callin@gmail.com>
2022-07-17 02:55:40 +0200

    actually working?

--- src/index.jsx
+++ src/index.jsx
@@ -1,64 +1,59 @@
 /* eslint-disable react/prop-types */
-import React, { useRef, useState, useLayoutEffect } from "react";
+import React, { useRef, useState, useCallback, useLayoutEffect } from "react";
 import { forwardRef } from "react";
 import videojs from "video.js";
 
-const VideoJsWrapper = React.memo(
-  forwardRef(
-    (
-      { children, videoJsOptions, onReady, onDispose, classNames, ...props },
-      player
-    ) => {
-      const videoNode = useRef(null);
-      const containerRef = useRef(null);
-      const changedKey = JSON.stringify(videoJsOptions);
+const VideoJsWrapper = forwardRef(
+  (
+    { children, videoJsOptions, onReady, onDispose, classNames, ...props },
+    player
+  ) => {
+    const videoJsOptionsCloned = JSON.parse(JSON.stringify(videoJsOptions));
+    const videoNode = useRef(null);
+    const containerRef = useRef(null);
+    const changedKey = JSON.stringify(videoJsOptionsCloned);
 
-      const mounted = useRef(true);
+    useLayoutEffect(() => {
+      if (!videoNode.current.parentNode) return;
+      const before = videoNode.current.parentNode.cloneNode(true);
+      if (!player.current) {
+        player.current = videojs(videoNode.current, videoJsOptionsCloned);
+        player.current.ready(() => {
+          onReady();
+        });
+      }
 
-      useLayoutEffect(() => {
-        mounted.current = true;
-        const before = videoNode.current.parentNode.cloneNode(true);
-        if (!player.current) {
-          player.current = videojs(videoNode.current, videoJsOptions);
-          player.current.ready(() => {
-            onReady();
-          });
-        }
+      return () => {
+        if (player.current) {
+          player.current.dispose();
 
-        return () => {
-          mounted.current = false;
-          if (player.current) {
-            player.current.dispose();
-
-            if (
-              videoNode.current.parentNode &&
-              !containerRef.current.contains(videoNode.current.parentNode)
-            ) {
-              // issue is that this adds old nodes as well, and we need to clean them up first?
-              containerRef.current.appendChild(before);
-              videoNode.current = before.firstChild;
-            }
-            player.current = null;
-            // onDispose();
+          if (
+            videoNode.current.parentNode &&
+            !containerRef.current.contains(videoNode.current.parentNode)
+          ) {
+            containerRef.current.appendChild(before);
+            videoNode.current = before.firstChild;
           }
-        };
-      }, [changedKey]);
+          player.current = null;
+          onDispose();
+        }
+      };
+    }, [changedKey]);
 
-      return (
-        <div ref={containerRef}>
-          <div data-vjs-player>
-            <video
-              ref={videoNode}
-              className={`video-js ${classNames}`}
-              {...props}
-            >
-              {children}
-            </video>
-          </div>
+    return (
+      <div ref={containerRef}>
+        <div data-vjs-player>
+          <video
+            ref={videoNode}
+            className={`video-js ${classNames}`}
+            {...props}
+          >
+            {children}
+          </video>
         </div>
-      );
-    }
-  )
+      </div>
+    );
+  }
 );
 
 VideoJsWrapper.displayName = "VideoJsWrapper";
@@ -68,20 +63,23 @@ export const useVideoJS = (videoJsOptions, classNames = "") => {
 
   const player = useRef(null);
 
-  const Video = ({ children, ...props }) => {
-    return (
-      <VideoJsWrapper
-        ref={player}
-        videoJsOptions={videoJsOptions}
-        classNames={classNames}
-        onReady={() => setReady(true)}
-        onDispose={() => setReady(false)}
-        {...props}
-      >
-        {children}
-      </VideoJsWrapper>
-    );
-  };
+  const Video = useCallback(
+    ({ children, ...props }) => {
+      return (
+        <VideoJsWrapper
+          ref={player}
+          videoJsOptions={videoJsOptions}
+          classNames={classNames}
+          onReady={() => setReady(true)}
+          onDispose={() => setReady(false)}
+          {...props}
+        >
+          {children}
+        </VideoJsWrapper>
+      );
+    },
+    [JSON.stringify(videoJsOptions), classNames]
+  );
 
   return { Video, ready, player: player.current };
 };
