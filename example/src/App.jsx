b7c5b63a663f70e51b9d1b3b9c6adc6900371433
Jimmy Callin <jimmy.callin@gmail.com>
2022-07-19 15:49:15 +0200

    Support React 18 including strict mode (#155)
    * react 18 support
    
    * remove node 12 support and bump vite to 3
    
    * bump all dependencies to latest

--- example/src/App.jsx
+++ example/src/App.jsx
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
 import "./App.css";
-
-import { useVideoJS } from "react-hook-videojs";
+import sample from "./sample.vtt?url";
 import "video.js/dist/video-js.css";
+import { useVideoJS } from "../../src/index";
 
 const App = () => {
   const [source, setSource] = useState("//vjs.zencdn.net/v/oceans.mp4");
+  const [vtt, setVtt] = useState(true);
   const [controls, setControls] = useState(true);
   const [autoplay, setAutoplay] = useState(false);
+  const [isMount, setIsMount] = useState(true);
 
   const videoJsOptions = {
     sources: [{ src: source }],
@@ -19,7 +21,19 @@ const App = () => {
   console.log({ Video, ready, player });
   return (
     <>
-      <Video />
+      {isMount && (
+        <Video>
+          {vtt ? (
+            <track
+              kind="captions"
+              src={sample}
+              srcLang="en"
+              label="English"
+              default
+            />
+          ) : null}
+        </Video>
+      )}
       <div style={{ display: "flex", flexDirection: "column", margin: "20px" }}>
         <label>
           Video source
@@ -28,6 +42,14 @@ const App = () => {
             type="text"
             value={source}
             onChange={(e) => setSource(e.target.value)}
+          />
+        </label>
+        <label>
+          VTT
+          <input
+            type="checkbox"
+            checked={vtt}
+            onChange={(e) => setVtt(e.target.checked)}
           />
         </label>
         <label>
@@ -44,6 +66,14 @@ const App = () => {
             type="checkbox"
             checked={autoplay}
             onChange={(e) => setAutoplay(e.target.checked)}
+          />
+        </label>
+        <label>
+          Mounted
+          <input
+            type="checkbox"
+            checked={isMount}
+            onChange={() => setIsMount((m) => !m)}
           />
         </label>
       </div>
