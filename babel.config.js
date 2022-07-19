be4b9a2c62ef1169bd4bd14a291f886a9d9d0929
Jimmy Callin <jimmy.callin@gmail.com>
2022-07-19 18:29:20 +0200

    remove babel dependencies

--- babel.config.js
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line no-undef
-module.exports = {
-  presets: [
-    ["@babel/preset-env", { targets: { node: "current" } }],
-    "@babel/preset-react",
-  ],
-  plugins: ["@babel/plugin-transform-typescript"],
-};
